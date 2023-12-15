1ddf77272ff745f5b78f2438eadcfd93331f1816
ToporekDaniel <danieltoporek0@gmail.com>
2023-12-16 00:00:26 +0100

    api pobiera obrazy i info

--- src/index.js
+++ src/index.js
@@ -9,8 +9,29 @@ const more = document.querySelector('.load-more');
 const input = document.querySelector('input[name="searchQuery"]');
 const baseURL = 'https://pixabay.com/api/';
 
+// const image = `
+//   <div class="photo-card">
+//     <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
+//     <div class="info">
+//       <p class="info-item">
+//         <b>Likes ${hit.likes}</b>
+//       </p>
+//       <p class="info-item">
+//         <b>Views ${hit.views}</b>
+//       </p>
+//       <p class="info-item">
+//         <b>Comments ${hit.comments}</b>
+//       </p>
+//       <p class="info-item">
+//         <b>Downloads ${hit.downloads}</b>
+//       </p>
+//     </div>
+//   </div>
+// `;
+
 form.addEventListener('submit', event => {
   event.preventDefault();
+  gallery.innerHTML = '';
   const word = input.value;
   console.log(word);
 
@@ -28,10 +49,44 @@ form.addEventListener('submit', event => {
 
   axios
     .get(baseURL, options)
-    .then(response => {
-      console.log('Response:', response.data);
+    .then(res => {
+      console.log('Response:', res.data);
+      console.log(res.data.hits);
+      res.data.hits.forEach(hit => {
+        console.log(hit.likes);
+        gallery.insertAdjacentHTML(
+          'beforeend',
+          `
+          <div class="photo-card">
+    <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes ${hit.likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views ${hit.views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments ${hit.comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads ${hit.downloads}</b>
+      </p>
+    </div>
+  </div>
+`
+        );
+      });
     })
     .catch(error => {
       console.error('Error:', error);
     });
 });
+
+// webformatURL - link do małego obrazka.
+// largeImageURL - link do dużego obrazka.
+// tags - wiersz z opisem obrazka. Będzie pasować do atrybutu alt.
+// likes - liczba “lajków”.
+// views - liczba wyświetleń.
+// comments - liczba komentarzy.
+// downloads - liczba pobrań.
